a98ab1240b6e70448bf6d8c63c732899715c4994
tnyenvy <tnyenvy.2003@gmail.com>
2024-11-28 00:17:04 +0700

    doi thu vien

--- Maze.py
+++ Maze.py
@@ -1,13 +1,4 @@
-# Tài liệu tham khảo
-# A. Artasanchez, P. Joshi, 
-# Artificial Intelligence with Python, 
-# 2nd Edition, Packt, 2020
-# Trang 242
-
-from simpleai.search import CspProblem, backtrack
-# Define the function that imposes the constraint 
-# that neighbors should be different
-
+import streamlit as st
 import math
 from simpleai.search import SearchProblem, astar
 
@@ -39,7 +30,6 @@ MAP = """
 
 # Class containing the methods to solve the maze
 class MazeSolver(SearchProblem):
-    # Initialize the class 
     def __init__(self, board):
         self.board = board
         self.goal = (0, 0)
@@ -53,75 +43,70 @@ class MazeSolver(SearchProblem):
 
         super(MazeSolver, self).__init__(initial_state=self.initial)
 
-    # Define the method that takes actions
-    # to arrive at the solution
     def actions(self, state):
         actions = []
         for action in COSTS.keys():
             newx, newy = self.result(state, action)
             if self.board[newy][newx] != "#":
                 actions.append(action)
-
         return actions
 
-    # Update the state based on the action
     def result(self, state, action):
         x, y = state
-
-        if action.count("up"):
+        if action == "up":
             y -= 1
-        if action.count("down"):
+        elif action == "down":
             y += 1
-        if action.count("left"):
+        elif action == "left":
             x -= 1
-        if action.count("right"):
+        elif action == "right":
             x += 1
 
         new_state = (x, y)
-
         return new_state
 
-    # Check if we have reached the goal
     def is_goal(self, state):
         return state == self.goal
 
-    # Compute the cost of taking an action
     def cost(self, state, action, state2):
         return COSTS[action]
 
-    # Heuristic that we use to arrive at the solution
     def heuristic(self, state):
         x, y = state
         gx, gy = self.goal
-
         return math.sqrt((x - gx) ** 2 + (y - gy) ** 2)
 
-if __name__ == "__main__":
-    # Convert map to a list
-    print(MAP)
-    MAP = [list(x) for x in MAP.split("\n") if x]
+# Streamlit UI
+st.title("Maze Solver")
 
-    # Create maze solver object
-    problem = MazeSolver(MAP)
+# Display the maze map
+st.text_area("Maze Map", MAP, height=200)
 
-    # Run the solver
-    result = astar(problem, graph_search=True)
+# Convert map to a list
+MAP = [list(x) for x in MAP.split("\n") if x]
 
-    # Extract the path
-    path = [x[1] for x in result.path()]
+# Create maze solver object
+problem = MazeSolver(MAP)
 
-    # Print the result
-    print()
-    for y in range(len(MAP)):
-        for x in range(len(MAP[y])):
-            if (x, y) == problem.initial:
-                print('o', end='')
-            elif (x, y) == problem.goal:
-                print('x', end='')
-            elif (x, y) in path:
-                print('·', end='')
-            else:
-                print(MAP[y][x], end='')
+# Run the solver
+result = astar(problem, graph_search=True)
 
-        print()
+# Extract the path
+path = [x[1] for x in result.path()]
 
+# Display the solved maze
+st.subheader("Solved Maze")
+solved_maze = ""
+for y in range(len(MAP)):
+    for x in range(len(MAP[y])):
+        if (x, y) == problem.initial:
+            solved_maze += 'o'
+        elif (x, y) == problem.goal:
+            solved_maze += 'x'
+        elif (x, y) in path:
+            solved_maze += '·'
+        else:
+            solved_maze += MAP[y][x]
+    solved_maze += "\n"
+
+st.text_area("Solution", solved_maze, height=300)
